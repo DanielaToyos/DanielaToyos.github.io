e7649b83b33b33b6dc8ff0fa5aee481a4b6cdf1f
DanielaToyos <danietob8@gmail.com>
2022-08-15 19:54:15 -0300

    edite js

---
+++ products.js
@@ -0,0 +1,14 @@
+const direccion = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    fetch(direccion)
+    .then(respuesta => respuesta.json());
+    .then(datos => {
+        document.getElementById('nombre').innerHTML=products.name;
+        document.getElementById('descripcion').innerHTML=procucts.description;
+        document.getElementById('costo').innerHTML=products.cost;
+        document.getElementById('moneda').innerHTML=products.currency;
+        document.getElementById('vendidos').innerHTML=products.soldCount;
+    });
+})
