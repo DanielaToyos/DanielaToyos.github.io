1526a4406c910603d466399fc426dac64cacd251
DanielaToyos <danietob8@gmail.com>
2022-08-17 19:53:39 -0300

    edite js

--- products.js
+++ products.js
@@ -1,14 +1,29 @@
 const direccion = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-document.addEventListener('DOMContentLoaded', ()=>{
+let categoriesArray = [];
 
-    fetch(direccion)
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        document.getElementById('nombre').innerHTML=products.name;
-        document.getElementById('descripcion').innerHTML=procucts.description;
-        document.getElementById('costo').innerHTML=products.cost;
-        document.getElementById('moneda').innerHTML=products.currency;
-        document.getElementById('vendidos').innerHTML=products.soldCount;
-    });
-})
+
+for(let i = 0; i < array.length; i++){ 
+    let category = array[i];
+    htmlContentToAppend +=`
+    <div class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                    <h4>`+ category.name +`</h4> 
+                    <p> `+ category.description +`</p> 
+                    </div>
+                    <small class="text-muted">` + category.productCount + ` artículos</small> 
+                </div>
+
+            </div>
+        </div>
+    </div>
+    ` 
+    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+}
+}
