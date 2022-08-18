a34066e229ec0aeaae139d2cdf8efe42d02bba26
DanielaToyos <danietob8@gmail.com>
2022-08-18 14:42:05 -0300

    edite html y js login

---
+++ login.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //Boton para registrarse
+    document.getElementById("regBtn").addEventListener("click", () => {
+        register();
+
+
+
+    })
+});
+
+function loginvalidate() {
+    //parrafo de los errores
+    let errors = document.querySelector('#errors');
+    let passerror = document.querySelector('#passerror');
+    let completo = document.querySelector('#completo');
+
+    let email = document.getElementById("floatingInput").value;
+    let password = document.getElementById("floatingPassword").value;
+
+    if (email === "" || (!email.includes("@") || !email.includes(".com")) || password === undefined) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+}
+
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
