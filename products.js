09318a2d60e30ee369bf24e8461abf0c4c98af62
DanielaToyos <danietob8@gmail.com>
2022-08-19 20:09:04 -0300

    fwkheku

--- products.js
+++ products.js
@@ -1,28 +1,43 @@
 const direccion = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-let categoriesArray = [];
 
+function showProductsList(){
 
-for(let i = 0; i < array.length; i++){ 
-    let category = array[i];
-    htmlContentToAppend +=`
-    <div class="list-group-item list-group-item-action">
-        <div class="row">
-            <div class="col-3">
-                <img src="` + category.imgSrc + `" alt="product image" class="img-thumbnail">
-            </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <div class="mb-1">
-                    <h4>`+ category.name +`</h4> 
-                    <p> `+ category.description +`</p> 
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentCategoriesArray.length; i++){
+        let category = currentCategoriesArray[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+
+            htmlContentToAppend += `
+            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
                     </div>
-                    <small class="text-muted">` + category.productCount + ` artículos</small> 
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${category.name}</h4>
+                            <small class="text-muted">${category.productCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${category.description}</p>
+                    </div>
                 </div>
-
             </div>
-        </div>
-    </div>
-    ` 
-    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+            `
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
 }
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    fetch(direccion)
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        console.log(datos);
+    })
+})
