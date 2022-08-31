f1638474feb52d4d858cfe6507ebc3c3b60d56f0
DanielaToyos <danietob8@gmail.com>
2022-08-30 22:06:21 -0300

    gvhgv

--- products.js
+++ products.js
@@ -38,11 +38,48 @@ function sortProducts(criteria, array){
 }
 
 
+document.addEventListener("DOMContentLoaded", function(e) {
+    let id=localStorage.getItem("catID");
+    getJSONData(PRODUCTS_URL + id + ".json").then(function (resultObj){ 
+    })
+})
+
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < productsArray.products.length; i++){ 
+        let products = productsArray.products[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img scr="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ products.name + '-' + products.currency + + products.cost +`</h4> 
+                        <p> `+ products.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + products.soldCount + ` vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+
 function sortAndShowProducts(sortCriteria, productsArray){
     currentSortCriteria = sortCriteria;
 
-    if(productsArray != undefined){
-        currentProductsArray = productsArray;
+    if(productsArray.products != undefined){
+        currentProductsArray = productsArray.products;
     }
 
     currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
@@ -50,6 +87,21 @@ function sortAndShowProducts(sortCriteria, productsArray){
     //Muestro las categorías ordenadas
     showProductsList();
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    let id=localStorage.getItem("catID");
+    getJSONData(PRODUCTS_URL + id + ".json").then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data;
+
+            showProductsList(productsArray);
+        }
+    });
+})
+
+
 
 document.getElementById("sortAsc").addEventListener("click", function(){
     sortAndShowProducts(ORDER_ASC_BY_NAME);
@@ -96,63 +148,4 @@ document.getElementById("rangeFilterCount").addEventListener("click", function()
     showProductsList();
 });
 
-
-
-
-
-//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
-function showProductsList(array){
-    let htmlContentToAppend = "";
-
-    for(let i = 0; i < productsArray.products.length; i++){ 
-        let products = productsArray.products[i];
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img scr="` + products.image + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ products.name + '-' + products.currency + + products.cost +`</h4> 
-                        <p> `+ products.description +`</p> 
-                        </div>
-                        <small class="text-muted">` + products.soldCount + ` vendidos</small> 
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        `
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
-    }
-}
-
-
-/* 
-EJECUCIÓN:
-
--Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
--Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
--Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
-
-*/
-
-document.addEventListener("DOMContentLoaded", function(e) {
-    let id=localStorage.getItem("catID");
-    getJSONData(PRODUCTS_URL + id + ".json").then(function (resultObj){ 
-    })
-})
-
-document.addEventListener("DOMContentLoaded", function(e){
-    let id=localStorage.getItem("catID");
-    getJSONData(PRODUCTS_URL + id + ".json").then(function(resultObj){
-        if (resultObj.status === "ok"){
-            productsArray = resultObj.data;
-
-            showProductsList(productsArray);
-        }
-    });
-})
 
