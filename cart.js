63fbb4f633d744d709ac6742a20874febd694d6c
DanielaToyos <danietob8@gmail.com>
2022-10-13 21:28:11 -0300

    sjhgjh

--- cart.js
+++ cart.js
@@ -1,19 +1,30 @@
 let carrito = [];
 
 
+function multiplicar(cant) {
+    let i = 0; i < carrito.articles.length;
+    let costo = parseInt(carrito.articles.unitCost);
+    let cantidad = parseInt(document.getElementById("cantidad").value);
+
+    costo * cantidad;
+    return
+};
+
+
 function mostrarCarrito(array) {
 
     let articulos = "";
 
     for (let i = 0; i < carrito.articles.length; i++) {
-        let articulo = carrito.articles[i]; 
+        let articulo = carrito.articles[i];
 
         articulos += `
         <tr>
             <th scope="row"><img src="${articulo.image}" alt="product image" style="width: 25%"></th>
             <td>${articulo.name}</td>
             <td>${articulo.currency} ${articulo.unitCost}</td>
-            <td><input type="text" class="form-control" id="cantidad" placeholder="ingrese cantidad" required>${articulo.count}</td>
+            <td><input type="number" class="form-control" id="cantidad" placeholder="${articulo.count}" required></td>
+            <td>${articulo.currency} change="${multiplicar(cant)}"</td>
         </tr>
         `
 
