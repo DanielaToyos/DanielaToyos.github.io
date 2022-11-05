099a066c45ef03df358109c491118c6a619cc026
DanielaToyos <danietob8@gmail.com>
2022-11-05 16:58:28 -0300

    sehdfjh

--- my-profile.js
+++ my-profile.js
@@ -1,34 +1,74 @@
-let nombre = document.getElementById("nombre").value;
+
 let apellido = document.getElementById("apellido").value;
+let usuario = localStorage.getItem("usuario");
+let correo = localStorage.getItem("email");
 
 
-function correo(){
-    let correo = localStorage.getItem("email");
+
+function getItems() {
 
     document.getElementById("Email").value = correo;
+    document.getElementById("nombre").value = usuario;
 }
 
-function guardarDatos(){
+/*function guardarDatos(){
     if (nombre === "" || nombre === undefined || apellido === undefined || apellido === "") {
         localStorage.setItem("nombre" , nombre);
         localStorage.setItem("apellido" , apellido);
     }
-}
+}*/
 
 
+/*document.getElementById("validacionUsuario").addEventListener('submit', event => {
+    if (!this.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
 
+    }
+    document.body.classList.add('was-validated');
+    ['change', 'input'].forEach(evento => { document.body.addEventListener(evento, validaciones) })
 
+})*/
 
+    // Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
+    (function () {
+        'use strict'
 
+        // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+        var forms = document.querySelectorAll('.needs-validation')
 
+        // Bucle sobre ellos y evitar el envío
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
 
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    });
 
+    function setItems(){
+        let nombre = document.getElementById("nombre").value;
+        let email = document.getElementById("Email").value;
+        
+
+        document.getElementById("guardar").addEventListener("click", () => {
+            localStorage.setItem("usuario", nombre);
+            localStorage.setItem("email", email);
+            localStorage.setItem("apellido", apellido);
+            
+        });
+       
+    }
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    let usuario = localStorage.getItem("usuario");
 
-    if(usuario == null) {
+    if (usuario == null) {
         alert("No hay nadie loggeado");
         location.href = "login.html";
     } else {
@@ -41,11 +81,9 @@ document.addEventListener('DOMContentLoaded', () => {
         location.href = "login.html";
     })
 
-    correo();
+    getItems();
+    setItems();
 
-    document.getElementById("logBtn").addEventListener("click", () => {
-        guardarDatos();
-    })
 });
 
 
