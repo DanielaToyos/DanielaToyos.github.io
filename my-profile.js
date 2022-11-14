c25b8f03f00a76dd7e1e16383b544c1761ac25d7
DanielaToyos <danietob8@gmail.com>
2022-11-13 23:20:21 -0300

    sknfckjrn

--- my-profile.js
+++ my-profile.js
@@ -1,119 +1,53 @@
-
-let apellido = document.getElementById("apellido").value;
 let usuario = localStorage.getItem("usuario");
-let correo = localStorage.getItem("email");
-let segundoApellido = document.getElementById("apellido2").value;
-let segundoNombre = document.getElementById("nombre2").value;
+let email = localStorage.getItem("email");
+let nombre = document.getElementById("nombre").value;
+let nombre2 = document.getElementById("nombre2").value;
+let apellido = document.getElementById("apellido").value;
+let apellido2 = document.getElementById("apellido2").value;
+let correo = document.getElementById("Email").value;
 let telefono = document.getElementById("telefono").value;
 
-let errors = document.querySelector("#errors");
-let passerror = document.querySelector("#passerror");
-let completo = document.querySelector("#completo");
 
-/*function uservalidate(){
-    if (usuario === "" || usuario === undefined) {
-        errors.textContent = "Ingrese un nombre";
-    } localStorage.setItem("usuario" , usuario);
-}
+function profilevalidate() {
 
-function apellidovalidate(){
-    if (apellido === "" || apellido === undefined) {
-        errors.textContent = "Ingrese un apellido";
-    } localStorage.setItem("apellido" , apellido);
-}
-
-function correovalidate(){
-    if (correo === "" || correo === undefined) {
-        errors.textContent = "Ingrese un correo";
-    } localStorage.setItem("email" , email);
-}*/
-
-
-function validate() {
- 
-        
-        
-
-        completo.textContent = "Sesion iniciada correctamente.";
-        errors.textContent = '';
-        localStorage.setItem("usuario", usuario);
-        localStorage.setItem("email", correo);
+    if(nombre === "" || || apellido === "" || correo === ""){
+        alert("Por favor llenar los campos obligatorios");
+    } else {
+        alert("Los cambios han sido guardados con exito");
+        localStorage.setItem("usuario" , nombre);
         localStorage.setItem("apellido" , apellido);
-        localStorage.setItem("segundoNombre" , segundoNombre);
-        localStorage.setItem("segundoApellido", segundoApellido);
+        localStorage.setItem("email" , correo);
+        localStorage.setItem("nombre2" , nombre2);
+        localStorage.setItem("apellido2" , apellido2);
         localStorage.setItem("telefono" , telefono);
-        location.href = "index.html";
-    
+    }
 
 }
 
-
-function getItems() {
-
-    document.getElementById("Email").value = correo;
-    document.getElementById("nombre").value = usuario;
-   // document.getElementById("apellido").value = apellido;
-    //document.getElementById("nombre2").value = segundoNombre;
+function agarrarInfo() {
+    correo = localStorage.getItem("email");
+    nombre = localStorage.getItem("usuario");
+    apellido = localStorage.getItem("apellido");
+    nombre2 = localStorage.getItem("nombre2");
+    apellido2 = localStorage.getItem("apellido2");
+    telefono = localStorage.getItem("telefono");
 }
 
-/*function guardarDatos(){
-    if (nombre === "" || nombre === undefined || apellido === undefined || apellido === "") {
-        localStorage.setItem("nombre" , nombre);
-        localStorage.setItem("apellido" , apellido);
-    }
-}*/
 
 
-/*document.getElementById("validacionUsuario").addEventListener('submit', event => {
-    if (!this.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
+/*function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
 
-    }
-    document.body.classList.add('was-validated');
-    ['change', 'input'].forEach(evento => { document.body.addEventListener(evento, validaciones) })
-
-})*/
-
-// Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
-/*(function () {
-    'use strict'
-
-    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    var forms = document.querySelectorAll('.needs-validation')
-
-    // Bucle sobre ellos y evitar el envío
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
-            }, false)
-        })
-});*/
-
-/*function setItems() {
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("Email").value;
-
-
-    document.getElementById("guardar").addEventListener("click", () => {
-        localStorage.setItem("usuario", nombre);
-        localStorage.setItem("email", email);
-        localStorage.setItem("apellido", apellido);
-
-    });
-
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
 }*/
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    
 
-    if (usuario == null) {
+    if(usuario == null) {
         alert("No hay nadie loggeado");
         location.href = "login.html";
     } else {
@@ -126,16 +60,13 @@ document.addEventListener('DOMContentLoaded', () => {
         location.href = "login.html";
     })
 
-    getItems();
+    agarrarInfo();
 
     document.getElementById("guardar").addEventListener("click", () => {
-        //uservalidate();
-        //apellidovalidate();
-        //correovalidate();
-        validate();
+        profilevalidate();
+
     })
-
-});
-
+    
+})
 
 
